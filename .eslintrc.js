5d55e91209048d4816f2add0f1d6ee9f50ba5cd5
Joe Maller <joemaller@users.noreply.github.com>
2017-05-19 10:21:41 -0400

    Simplify eslint.js rules
    Relying on Prettify for most of this stuff now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,10 @@ module.exports = {
     sourceType: "module"
   },
   rules: {
-    'indent': ['error', 2 ],
-    'linebreak-style': ['error', 'unix'],
-    'no-console': 'warn',
-    'no-param-reassign': 'error',
-    'quotes': ['error', 'single', { allowTemplateLiterals: true }],
-    'semi': ['error', 'always'],
-    'space-before-function-paren': 'off',
-    'vars-on-top': 'off',
+    "linebreak-style": ["error", "unix"],
+    "no-console": "warn",
+    "no-param-reassign": "error",
+    "space-before-function-paren": "off",
+    "vars-on-top": "off"
   }
 };
